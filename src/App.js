06ff522a5303c3b6a785ec80a563f5778fd17f9b
aseelsaleh <aseel.saleh2611@gmail.com>
2020-12-05 12:17:57 +0200

    setup project

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import Navbar from './components/Navbar';
+import Planets from './components/Planets';
+import People from './components/People';
+
 
 function App() {
+  let [page, setPage] = useState('planets');
+
   return (
     <div className="App">
-    
+     <h1>Star Wars Info</h1>
+      <Navbar setPage={setPage} />
+      <div className="content">
+        { page === 'planets' ? <Planets /> : <People /> }
+      </div>
     </div>
   );
 }
